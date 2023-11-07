64d3d670c21837c1b79295b4380b45118a1bf7e7
Yahia Mhemdi <148154096+yahia-mhemdi-esprit@users.noreply.github.com>
2023-11-07 09:46:56 +0100

    Create donornew.c

---
+++ donornew.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "donation.h"
+
+int addDonation sheet(char *filename, struct Sheet s)
+{
+    FILE *f = fopen(filename, "a");
+    if (f != NULL)
+    {
+        fprintf(f, "%s %s %s %d %d %d %d %s\n",
+                s.id, s.fn, s.pn, s.dob ,s.doa, s.gender, s.others,s.btc);
+        fclose(f);
+        return 1;
+    }
+    else
+        return 0;
+}
